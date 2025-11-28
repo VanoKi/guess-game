0ecc2aea057d1dac28736062928d77f9e55cbb45
VanoKi <kirsvano@gmail.com>
2025-11-28 19:35:52 +0300

    feat: create array, get one random (2025-11-28 19:35:52)

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello world')
+const createArray = () => {
+    const arr= []
+    for (let i = 1; i < 10; i++) {
+        arr.push(i)
+    }
+    return arr
+}
+
+const arr = createArray()
+console.log(Math.trunc(Math.random()*(arr.length - 0)))
