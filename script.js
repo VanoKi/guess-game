d46b5c7f55476460def7f648255e08846ce2ea1e
VanoKi <kirsvano@gmail.com>
2025-11-28 19:49:13 +0300

    feat: shuffleArray (2025-11-28 19:49:13)

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ const createArray = () => {
 }
 
 const arr = createArray()
-console.log(Math.trunc(Math.random()*(arr.length - 0)))
+const shuffleArray = (arr) => {
+    const shuffleArray = []
+    const count = arr.length
+    for (let i = 0; i < count; i++) {
+        let n = (Math.trunc(Math.random()*(arr.length - 0)))
+        shuffleArray.push(arr[n])
+        arr.splice(n, 1)
+    }
+    return shuffleArray
+}
+console.log(shuffleArray(arr))
